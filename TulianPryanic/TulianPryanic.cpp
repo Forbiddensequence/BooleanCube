c710fdd11901857790c5bc779117fd389e67aafc
Forbiddensequence <70473598+Forbiddensequence@users.noreply.github.com>
2021-07-17 16:32:03 +0300

    Latest work version

--- TulianPryanic/TulianPryanic.cpp
+++ TulianPryanic/TulianPryanic.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <vector>
+#include <thread>
+
 using namespace std;
 long long int factorial(long long int a);
 
@@ -21,11 +23,7 @@ void start_factorial() {
 
 long long int poft(long long int power) {
     long long int a = 1;
-    for (long long int i = 0; i < power; i++) {
-        a = a << 1;
-        //cout << a << " И тут тоже \n";
-    }
-    return a;
+    return a<<power;
 }
 
 void initcube(long long int demension, vector<long long int>& vect) {
@@ -46,13 +44,8 @@ bool isinvector(long long int element, vector<long long int>& vect) {
     }
     return false;
 }
-/*
-void factorizationofnuberinvector(long long int number,vector<long long int>& vect) {
-    for (long long int i = 0;) {
 
-    }
-}
-*/
+
 
 
 long long int discretbinarylog(long long int y, long long int n) {
@@ -81,16 +74,77 @@ void initvectormask(long long int k,long long int n, vector <long long int>& fil
 }
 
 long long int fillingones(long long int n) {
-    int res=1;
+    long long int res=1;
     for (long long int i = 0; i < n-1; i++) {
         res = (res << 1) | 1;
     }
     return res;
 }
 
+long long int scalarmult(long long int v1, long long int v2) {
+    return weightofvector(v1 & v2) % 2;
+}
+
+long long int degreeof(long long int vector, long long int dem) {
+    long long int f = vector;
+    long long int degr = 0;
+    long long int newf = 0;
+    //cout<< hex << " Начало расчета степени для функции " <<  f << endl;
+    for (long long int i = 1; i < poft(dem); i++) {
+        
+        for (long long int j = 0; j < poft(dem) - i ; j++) {
+            newf |= ((((f >> j) & 1) ^ ((f >> (j + 1)) & 1)) << j);
+        }
+        f = newf;
+        newf = 0;
+        if ((f & 1)&&(degr< weightofvector(i))) {
+            degr = weightofvector(i);
+        }
+        //cout<<hex<<f<<" - Значение На итерации под номером "<<i<<endl;
+    }
+
+    //cout << hex << "Конец расчета степени, она равна " <<degr<< endl;
+    return degr;
+}
+
+long long int AI(long long int vector, long long int dem) {
+    long long int AIdegree = dem;
+    long long int base = vector;
+    for (int inv = 0; inv < 2; inv++) {
+
+        //cout << hex << "Функция, для которой определяем имунность (База) равна " << base<< endl;
+        base = fillingones(poft(dem)) ^ base;
+        for (long long int i = 0; i < poft(weightofvector(base)); i++) {
+            long long int currentpoint = 0;
+            long long int almostpoint = base;
+            long long int dby = i;
+            for (long long int z = 0; z < poft(dem); z++) {
+                if (almostpoint & 1) {
+                    currentpoint = ((dby & 1) << z) | currentpoint;
+                    dby = dby >> 1;
+                }
+                almostpoint = almostpoint >> 1;
+            }
+            if (currentpoint==0) {
+                continue;
+            }
+            long long int res= degreeof(currentpoint, dem);
+            if (AIdegree > res) {
+                AIdegree = res;
+            }
+            if (AIdegree == 1) {
+                //cout << hex << "Аннулятор функции " << vector << " или же " << (fillingones(poft(dem))^vector) << " - " << currentpoint << endl;
+                return AIdegree;
+            }
+        }
+    }
+    return AIdegree;
+}
+
+
 bool isCIorderequals(long long int function, long long int k, long long int n) {
     long long int m = n - k;
-    long long int boundaryofunfixedvariable =poft(m)* numberofpermutation(k,n);
+    long long int boundaryofunfixedvariable =poft(m) * numberofpermutation(k,n);
 
     //cout << "Значение количества всех возможных граней размерности "<< m <<" в "<< n <<"-мерном кубе - "<< boundaryofunfixedvariable << "\n";
 
@@ -151,39 +205,69 @@ bool isCIorderequals(long long int function, long long int k, long long int n) {
     return isKorder;
 }
 
-long long int CI(long long int vector,int dem) {
-    int orderofCI = 0;
-    for (long long int j = 1; j < dem; j++) {
-        bool res = isCIorderequals(vector, j, dem);
+long long int CI(long long int vector, long long int dem) {
+    int orderofCI = -1;
+    //bool res = true;
+    for (long long int j = 1; (j < dem)&& isCIorderequals(vector, j, dem); j++) {
+        //res = ;
         //cout << res << "\n";
-        if (res) {
-            orderofCI = j;
-        }
+        orderofCI = j;
     }
+    return orderofCI;
+    /*
     if (orderofCI) {
         return orderofCI;
     }
-    else {
-        return -1;
-    }
+    return -1;
+    */
 }
 
-
-long long int showCIinrange(int basedem) {
+long long int showCIandAIinrange(long long int basedem, long long int beginpoint, long long int endpoint) {
     long long int ctr=0;
-    for (int i = 0; i < poft(poft(basedem));i++) {
+    for (int i = beginpoint; i < endpoint;i++) {
         /*if (i == poft(poft(basedem))>>1) {
             i=i;
         }*/
+        if (i==0|| i == fillingones(poft(basedem))||weightofvector(i)!=16) {
+            continue;
+        }
         int orderofCI = CI(i,basedem);
-        if (orderofCI>0) {
-            ctr++;
-            cout << hex << "Корреляционная имунность функции " << i << " равна - " << orderofCI << "\n";
+        if (orderofCI>0 ) {
+            int orderofAI = AI(i, basedem);
+            //ctr++;
+            if ((orderofAI >= 3) ) {
+                ctr++;
+                cout << hex << "Корреляционная имунность функции " << i << " равна - " << orderofCI << ", Алгебраическая " << orderofAI << ", Вес функции равен "<<weightofvector(i)<<endl;
+            }
         }
     }
     cout << dec << "Итоговое количество подходящих функций  равно - " << ctr << "\n";
     return 0;
 }
+
+/*
+long long int testpat(long long int basedem, long long int subdem) {
+    vector <long long int> vecfunction;
+    for (long long int i = 0; i <poft(subdem); i++) {
+        if (weightofvector(i)==poft(subdem-1)) {
+            vecfunction.push_back(i);
+        }
+
+    }
+
+    for (long long int i = 0; i < poft(basedem-subdem); i++) {
+        
+
+    }
+
+
+
+    return 0;
+}
+
+
+
+*/
 
 void pat1(long long int n, long long int beginpoint ) {
     vector <long long int> vecbeginedge;
@@ -203,7 +287,7 @@ void pat1(long long int n, long long int beginpoint ) {
     long long int ctr = 0;
     for (long long int i = 0; i < direction.size(); i++) {
         long long int f = functionnumber + poft(vecbeginedge[0] ^ direction[i]) + poft(vecbeginedge[1] ^ direction[i]);
-        if (true||CI(f, n)>0) {
+        if (CI(f, n)>0) {
             cout << hex << f << " - Функция по паттерну 1\n";
             cout << dec << CI(f, n) << "- Корреляционная иммунность функции " << hex << f << " по паттерну 1 \n";
             
@@ -215,52 +299,6 @@ void pat1(long long int n, long long int beginpoint ) {
 
 }
 
-long long int degreeof(long long int vector, long long int dem) {
-    long long int f = dem;
-    long long int degr = 0;
-    long long int newf = 0;
-    for (long long int i = 0; i < exp(dem); i++){
-        if (f & 1) {
-            degr = weightofvector(i);
-        }
-        for (long long int j = 0;j<exp(dem) - i-1;j++) {
-            newf|=(((vector >> j & 1) ^ (vector >> (j + 1) & 1))<<j);
-        }
-        f = newf;
-        newf = 0;
-    }
-    return degr;
-}
-
-long long int scalarmult(long long int v1, long long int v2) {
-    long long int res = 0;
-    while ((v1>0)&&(v2>0)) {
-        res+=v1&1&
-    }
-}
-
-long long int AI(long long int vector, long long int dem) {
-    long long int base = fillingones(poft(dem))^vector;
-    long long int AIdegree = 0;
-    
-    for (long long int i = 0; i < poft(weightofvector(base));i++) {
-        long long int currentsubpoint = 0;
-        long long int almostsubpoint = base;
-        long long int dby = i;
-        for (long long int z = 0; z < poft(dem); z++) {
-            if (almostsubpoint & 1) {
-                currentsubpoint = ((dby & 1) << z) | currentsubpoint;
-                dby = dby >> 1;
-            }
-            almostsubpoint = almostsubpoint >> 1;
-        }
-
-
-    }
-    
-    return 0;
-}
-
 int main()
 { 
     setlocale(LC_ALL,"ru");
@@ -269,8 +307,8 @@ int main()
     vector <long long int> veccube;
     vector <long long int>::iterator it;
     vector <long long int> vecfunction;
-    long long int dem = 4;
-    //cout << sizeof(dem)<<"\n";
+    long long int dem = 5;
+    cout << sizeof(dem)<<"\n";
     initcube(dem,veccube);
     /*
     cout << "Значения вектора куба " << "\n";
@@ -278,13 +316,13 @@ int main()
         cout << *it<<" Вес - "<< weightofvector(*it)<<"\n";
     }
     */
-    long long int funcnumber = poft(8) + poft(15);// +poft(19) + poft(28);
+    long long int funcnumber = poft(7) + poft(8);
     //cout <<hex<< poft(64)-poft(0)<< "\n";
     cout <<dec<< funcnumber<< " Вес - " << weightofvector(funcnumber) << "\n";
     for (it = vecfunction.begin(); it != vecfunction.end(); ++it) {
         cout << *it << "\n";
     }
-    
+   
     
     int orderofCI = 0;
     for (long long int i  = 1; i < dem;i++) {
@@ -294,13 +332,40 @@ int main()
             orderofCI = i;
         }
     }
-
     cout << hex <<"Корреляционная имунность функции "<< funcnumber << " равна - " << orderofCI << "\n";
+    //cout << hex <<"Алгебраическая имунность функции "<< funcnumber << " равна - " << AI(funcnumber,dem) << "\n";
     
+    /*
     for (int i = 0; i < poft(6); i++) {
         pat1(6,i);
     }
-    showCIinrange(dem);
+    */
+    long long int frac = thread::hardware_concurrency();
+    long long int partition= (poft(poft(dem)-1)) / frac;
+    long long int shift=0xe74742e;
+    vector <thread> threads;
+    vector <long long int> shifts;
+    //cout << dec<< " Параметр Hc равен - " << thread::hardware_concurrency() << endl;
+
+    for (long long int i = 0; i < frac; i++) {
+        shifts.push_back(0);
+    }
+    shifts[0] = 0x17e4c3a9;
+    shifts[1] = 0xc76794a;
+    shifts[2] = 0xb9a71c9;
+    shifts[3] = 0x65ce12b;
+    for (long long int i = 0; i<frac;i++) {
+        threads.push_back(thread(showCIandAIinrange,dem, i * partition+shifts[i], (i+1) * partition));
+        //threads.push_back(thread(test, dem, i * partition, (i + 1) * partition));
+    }
+
+    for (long long int i = 0; i < threads.size();i++) {
+        threads[i].join();
+    }
+
+
+
+
     //cout << "Хаха у тебя ничего не выводится ";
     return 0;
 }
